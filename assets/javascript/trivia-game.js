e4c49b43e65ce235522b1d1c30e2cc28951cd18a
PatriciaRippe14 <p.richmond0403@gmail.com>
2018-06-19 22:12:54 -0600

    updated code

--- assets/javascript/trivia-game.js
+++ assets/javascript/trivia-game.js
@@ -42,21 +42,21 @@ $(document).ready(function() {
         unansweredTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);
+        setTimeout(wait, 3000);
     }
     
     function generateWin() {
         correctTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000);
+        setTimeout(wait, 3000);
     }
     
     function generateLoss() {
         incorrectTally++;
         gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='assets/images/x.png'>";
         $(".mainArea").html(gameHTML);
-        setTimeout(wait, 4000); 
+        setTimeout(wait, 3000); 
     }
     
     function generateHTML() {
